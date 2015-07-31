637fe7797c9ccf1af38178a7af55ab3681eed965
chris1103 <christian.kittler@gmx.de>
2015-07-31 20:33:42 +0200

    chriss

--- WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -11,13 +11,7 @@ namespace WindowsFormsApplication1
 
         public void lol()
         {
-<<<<<<< HEAD
-            chriiiis +1
-
-=======
-
-TOBIAS
->>>>>>> origin/master
+        christ
         }
     }
 }
